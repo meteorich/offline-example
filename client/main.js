6748673f8133867182d309c4b5ffb17460b543c5
Sands <richardgsands@gmail.com>
2017-08-14 22:22:41 +0100

    created logs collection and hooked into button and counter

--- client/main.js
+++ client/main.js
@@ -4,19 +4,20 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  this.subscribe('logs');
 });
 
 Template.hello.helpers({
   counter() {
-    return Template.instance().counter.get();
+    return Logs.find().count();
   },
+  logs() {
+    return Logs.find({}, { sort: { createdAt: -1 } });
+  }
 });
 
 Template.hello.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    Logs.insert({ createdAt: (new Date) });
   },
 });
