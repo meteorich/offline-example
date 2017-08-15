f81441ee295571bc0575a02d3bd88d8cfecbf779
Sands <richardgsands@gmail.com>
2017-08-15 08:21:11 +0100

    offline and online collection in view

--- client/main.js
+++ client/main.js
@@ -3,17 +3,25 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+_Logs = new Ground.Collection('_logs');
+_Logs.observeSource(Logs.find());
+
+
 Template.hello.onCreated(function helloOnCreated() {
   this.subscribe('logs');
+  
 });
 
 Template.hello.helpers({
   counter() {
-    return Logs.find().count();
+    return _Logs.find().count();
   },
   logs() {
+    return _Logs.find({}, { sort: { createdAt: -1 } });
+  },
+  logsOnline() {
     return Logs.find({}, { sort: { createdAt: -1 } });
-  }
+  }  
 });
 
 Template.hello.events({
@@ -21,3 +29,7 @@ Template.hello.events({
     Logs.insert({ createdAt: (new Date) });
   },
 });
+
+Template.registerHelper('formatDate', (d) => {
+  return moment(d).format('YYYY-MM-DD hh-mm-ss')
+})
